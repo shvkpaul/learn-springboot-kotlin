635449d2fecea422ee239bc18829cc447b69a399
Shouvik Paul <shouvik.paul@ah.nl>
2023-07-10 16:03:19 +0200

    initial setup

---
+++ src/main/kotlin/com/shvk/learn/learn/LearnSpringbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.shvk.learn.learn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnSpringbootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnSpringbootKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/shvk/learn/learn/LearnSpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shvk.learn.learn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnSpringbootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
